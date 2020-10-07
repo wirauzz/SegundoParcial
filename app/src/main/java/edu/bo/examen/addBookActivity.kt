04b445d6bbb47c9cdacfc0588194f2b929c03810
Wilson Fabricio Ramirez Sandy <wramirez995@gmail.com>
2020-10-07 10:39:23 -0400

    Fixed edittext blank fields

--- app/src/main/java/edu/bo/examen/addBookActivity.kt
+++ app/src/main/java/edu/bo/examen/addBookActivity.kt
@@ -2,6 +2,8 @@ package edu.bo.examen
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_add_book.*
 import kotlinx.coroutines.GlobalScope
@@ -13,19 +15,38 @@ class addBookActivity : AppCompatActivity() {
         setContentView(R.layout.activity_add_book)
 
         addButton.setOnClickListener{view ->
-            GlobalScope.launch {
-                val bookDao = AppRoomDatabase.getDatabase(applicationContext).bookDato()
-                val repository = BookRepository(bookDao)
-                repository.insert(Book(titleEditText.text.toString(),pagesEditText.text.toString(),editorialEditText.text.toString(),
-                    authorEditText.text.toString(),descriptionEditText.text.toString(),UrlImageEditText.text.toString()))
+            if(validate(listOf<EditText>(titleEditText, pagesEditText, editorialEditText, authorEditText, descriptionEditText, UrlImageEditText))) {
+                GlobalScope.launch {
+                    val bookDao = AppRoomDatabase.getDatabase(applicationContext).bookDato()
+                    val repository = BookRepository(bookDao)
+                    repository.insert(
+                        Book(
+                            titleEditText.text.toString(),
+                            pagesEditText.text.toString(),
+                            editorialEditText.text.toString(),
+                            authorEditText.text.toString(),
+                            descriptionEditText.text.toString(),
+                            UrlImageEditText.text.toString()
+                        )
+                    )
+                }
+                startActivity(Intent(this, MainActivity::class.java))
             }
-            startActivity(Intent(this, MainActivity::class.java))
         }
 
         cancelButton.setOnClickListener{
             val myIntent = Intent(applicationContext, MainActivity::class.java)
             startActivityForResult(myIntent, 0)
         }
+    }
 
+    fun validate(fields: List<EditText>): Boolean {
+        for (i in fields.indices) {
+            val currentField = fields[i]
+            if (currentField.text.isEmpty()) {
+                return false
+            }
+        }
+        return true
     }
 }
