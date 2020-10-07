a595ee09aad4062b10ef54ad757f20693c656603
Wilson Fabricio Ramirez Sandy <wramirez995@gmail.com>
2020-10-06 21:01:55 -0400

    Added activity

--- app/src/main/java/edu/bo/examen/MainActivity.kt
+++ app/src/main/java/edu/bo/examen/MainActivity.kt
@@ -1,9 +1,12 @@
 package edu.bo.examen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -17,7 +20,7 @@ class MainActivity : AppCompatActivity() {
             val bookDao =
                 AppRoomDatabase.getDatabase(applicationContext).bookDato()
             val repository = BookRepository(bookDao)
-            repository.insert(Book("Alguien","5", "Algo","alguien","desc","asdasd"))
+            repository.insert(Book("Otro","459", "846","somena","desc","asdasd"))
                 val lista = repository.getListBooks()
 
             bookRecyclerView.adapter = bookListAdapter(lista as ArrayList<Book>, applicationContext)
@@ -25,6 +28,11 @@ class MainActivity : AppCompatActivity() {
             var linearLayoutManager = LinearLayoutManager(applicationContext)
             linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
             bookRecyclerView.layoutManager = linearLayoutManager
+        }
+
+        val addFloatingButon: View = findViewById(R.id.floatingAddButon)
+        addFloatingButon.setOnClickListener {
+            startActivity(Intent(this, addBookActivity::class.java))
         }
     }
 }
